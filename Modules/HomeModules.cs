06b0f816606274b063dc2617dd10a4dac9d3e3a1
Kory Skarbek and grinil khanna <kory+grinil@>
2017-02-23 15:00:56 -0800

    set up delete restaurants method

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -8,24 +8,44 @@ namespace RestaurantsApp
     {
         public HomeModule()
         {
+            // Take you to the homepage
             Get["/"] = _ => {
                 return View["index.cshtml"];
             };
-
+            // take you to the cuisine page
             Get["/cuisines"] = _ => {
                 // var newCuisine = new Cuisine(Request.Form["cuisine-type"]);
                 // newCuisine.Save();
                 var cuisineList = Cuisine.GetAll();
                 return View["cuisines.cshtml", cuisineList];
             };
-
+            // Post the enter cuisine name onto the cuisines page
             Post["/cuisines"] = _ => {
                 var newCuisine = new Cuisine(Request.Form["cuisine-type"]);
                 newCuisine.Save();
                 var cuisineList = Cuisine.GetAll();
                 return View["cuisines.cshtml", cuisineList];
             };
+            // Delete a cuisine
+            Delete["/cuisines/{cuisineId}"] = parameters => {
+                Cuisine specificCuisine = Cuisine.Find(parameters.cuisineId);
+                specificCuisine.Delete();
+                List<Cuisine> cuisineList = Cuisine.GetAll();
+                return View["cuisines.cshtml", cuisineList];
+            };
+            // Edit a cuisine
+            Patch["/cuisines/{cuisineId}/updated"] = parameters => {
+                Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
+                selectedCuisine.Update(Request.Form["cuisine-type"]);
+                return View["cuisineUpdated.cshtml"];
+            };
+            // Take you to the page to edit a cuisine
+            Get["/cuisines/{cuisineId}/edit"] = parameters => {
+                Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
+                return View["cuisineEdit.cshtml", selectedCuisine];
+            };
 
+            // Get an id for each cuisine and take you to the clicked on cuisines restaurant page
             Get["/cuisines/{id}"] = parameters => {
                 Dictionary<string, object> model = new Dictionary<string, object>();
                 Cuisine selectedCuisine = Cuisine.Find(parameters.id);
@@ -34,6 +54,7 @@ namespace RestaurantsApp
                 model.Add("cuisine", selectedCuisine);
                 return View["restaurants.cshtml", model];
             };
+            // Take the restaurant entered and post in on the restaurants page
             Post["/cuisine/{id}/restaurants"] = parameters => {
                 Dictionary<string, object> model = new Dictionary<string, object>();
                 Cuisine selectedCuisine = Cuisine.Find(Request.Form["cuisine-id"]);
@@ -46,31 +67,6 @@ namespace RestaurantsApp
                 model.Add("cuisine", selectedCuisine);
                 return View["restaurants.cshtml", model];
             };
-
-            Get["/cuisines/{cuisineId}/edit"] = parameters => {
-                Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
-                selectedCuisine.Update(Request.Form["cuisine-type"]);
-                return View["cuisineEdit.cshtml"];
-            };
-
-            Patch["/cuisines/{cuisineId}/updated"] = parameters => {
-                Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
-                selectedCuisine.Update(Request.Form["cuisine-type"]);
-                return View["cuisineUpdated.cshtml"];
-            };
-
-            Get["/cuisines/{cuisineId}/edit"] = parameters => {
-                Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
-                return View["cuisineEdit.cshtml", selectedCuisine];
-            };
-
-            Delete["/cuisines/{cuisineId}"] = parameters => {
-                Cuisine specificCuisine = Cuisine.Find(parameters.cuisineId);
-                specificCuisine.Delete();
-                List<Cuisine> cuisineList = Cuisine.GetAll();
-                return View["cuisines.cshtml", cuisineList];
-            };
-
             Get["/cuisine/{id}/restaurant/{restaurantId}"] = parameters => {
                 Dictionary<string, object> model = new Dictionary<string, object>();
                 Cuisine selectedCuisine = Cuisine.Find(parameters.id);
@@ -78,11 +74,11 @@ namespace RestaurantsApp
                 List<Restaurant> allRestaurants = selectedCuisine.GetRestaurants();
                 model.Add("cuisine", selectedCuisine);
                 model.Add("restaurant", allRestaurants);
-                // model.Add("restaurantId", selectedRestaurant);
+                model.Add("restaurantId", selectedRestaurant);
                 return View["restaurants.cshtml", model];
             };
-
-            Post["/cuisines/{cuisineId}/restaurants/{restaurantId}"] = parameters => {
+            // Delete a restaurant from a cuisine
+            Delete["/cuisines/restaurants/{restaurantId}"] = parameters => {
                 Cuisine selectedCuisine = Cuisine.Find(parameters.cuisineId);
                 Restaurant specificRestaurant = Restaurant.Find(parameters.restaurantId);
                 specificRestaurant.Delete();
